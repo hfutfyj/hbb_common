3c3f90b059b29f144f8a07c8bd4d60db2ec51d2f
hfutfyj <hfutfyj@gmail.com>
2025-04-02 15:28:51 +0800

    修改默认的服务器、端口和key

--- src/config.rs
+++ src/config.rs
@@ -98,11 +98,11 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["nas.hfutfyj.eu.org"];
+pub const RS_PUB_KEY: &str = "XOcfLKbkAN3bQ7+PWejSKA6j1gJYjDUT7l3f078NNSg=";
 
-pub const RENDEZVOUS_PORT: i32 = 21116;
-pub const RELAY_PORT: i32 = 21117;
+pub const RENDEZVOUS_PORT: i32 = 36000;
+pub const RELAY_PORT: i32 = 36001;
 
 macro_rules! serde_field_string {
     ($default_func:ident, $de_func:ident, $default_expr:expr) => {
